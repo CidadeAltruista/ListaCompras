d05db56b6f09536bc68da0c7c77c701205d67692
CidadeAltruista <cidadealtruista@gmail.com>
2025-06-18 19:02:08 +0100

    1

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const SHEET_ID = '1som9-aGql45SsU5LwkT--dhXeiT8S4Vs6w838_DzT7U';
-const API_URL = 'https://script.google.com/macros/s/AKfycby6Bb4Q1qMnpGtXZVb3LxdmmeqbESEZn_PFRqjEDKdnMIkbHG8OI7M6iYy6GflecHrn/exec'; // <- atualiza aqui
+const API_URL = 'https://script.google.com/macros/s/AKfycbxgVfBNQhn7npWuc9m0LlSmC6F08yYGLS0ULLqt4UvnjbBeuKhI39rWn13fleeflCo50g/exec'; // <- atualiza aqui
 
 export async function obterDados() {
   const res = await fetch(`${API_URL}?sheet=Lista`);
@@ -9,8 +9,9 @@ export async function obterDados() {
 export async function atualizarCelula(linha, coluna, valor) {
   const res = await fetch(API_URL, {
     method: 'POST',
-    body: JSON.stringify({ linha, coluna, valor }),
-    headers: { 'Content-Type': 'application/json' }
+    headers: { 'Content-Type': 'text/plain' }, // ← IMPORTANTE!
+    body: JSON.stringify({ linha, coluna, valor })
   });
   return await res.json();
 }
+
